f940dda015bb56af710bf9614cb83f8e457010e9
mostafa-m-desouky <mostafa.m.desouky@gmail.com>
2024-05-24 21:54:34 +0200

    register

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const config = require("./config")
 const cors = require('cors')
 const dotenv = require('dotenv')
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
 const mongoose = require('mongoose');
 const app = express()
 
@@ -9,17 +13,12 @@ dotenv.config();
 app.use(express.json());
 
 mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify:true
-  })
+  .connect(process.env.MONGO_URL)
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
-// app.use("/", (req, res) => {
-//     res.end()
-// })
+
+
+app.use("/api/auth", authRoute);
 
 app.listen("5000", () => {
     console.log("running on Port: 5000")
