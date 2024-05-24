fa266370fca888f6b3d4f3dd10c1502ad6bed131
mostafa-m-desouky <mostafa.m.desouky@gmail.com>
2024-05-24 21:01:40 +0200

    connect with mongoDB

--- server/index.js
+++ server/index.js
@@ -1,5 +1,24 @@
 const express = require('express')
 const config = require("./config")
 const cors = require('cors')
+const dotenv = require('dotenv')
+const mongoose = require('mongoose');
+const app = express()
 
+dotenv.config();
 app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify:true
+}).then(console.log("Connected to MongoDB"))
+.catch((err) => console.log(err))
+// app.use("/", (req, res) => {
+//     res.end()
+// })
+
+app.listen("5000", () => {
+    console.log("running on Port: 5000")
+})
