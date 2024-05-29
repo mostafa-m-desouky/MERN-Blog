4d749782b4a9653d54f3e3b0d2b4af1c32faa4a5
mostafa-m-desouky <mostafa.m.desouky@gmail.com>
2024-05-29 22:58:45 +0200

    posts & categories and upload images

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const express = require('express')
-const config = require("./config")
 const cors = require('cors')
 const dotenv = require('dotenv')
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
+const multer = require("multer");
+const path = require("path");
 const mongoose = require('mongoose');
 const app = express()
 
@@ -18,11 +19,26 @@ mongoose
   .catch((err) => console.log(err));
 
 
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+  
+const upload = multer({ storage: storage });
+  app.post("/api/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("File has been uploaded");
+});
+
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
 
-app.listen("5000", () => {
-    console.log("running on Port: 5000")
+app.listen(process.env.PORT, () => {
+    console.log("Backend is running.")
 })
