b8e80cc2043f884f88c3539e26dafcba9f7de67f
Tosin567 <oluwatosinjegede1@gmail.com>
2024-05-30 11:27:48 -0700

    Tosin

--- client/src/pages/login/Login.js
+++ client/src/pages/login/Login.js
@@ -6,7 +6,7 @@ export default function Login() {
       <span className="loginTitle">Login</span>
       <form className="loginForm">
         <label>Email</label>
-        <input className="loginInput" type="text" placeholder="Enter your email..." />
+        <input className="loginInput" type="text" placeholder="Enter your e-mail..." />
         <label>Password</label>
         <input className="loginInput" type="password" placeholder="Enter your password..." />
         <button className="loginButton">Login</button>
